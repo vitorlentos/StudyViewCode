6421c117b387064d6da09de395d78d0a487df594
Vitor Lentos <lentos.exe@gmail.com>
2021-11-02 15:16:33 -0300

    0.2
    modify Project to ViewCode

---
+++ ViewCodeOne/Util/Util.h
@@ -0,0 +1,18 @@
+//
+//  Util.h
+//  Util
+//
+//  Created by Vitor Lentos on 02/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Util.
+FOUNDATION_EXPORT double UtilVersionNumber;
+
+//! Project version string for Util.
+FOUNDATION_EXPORT const unsigned char UtilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Util/PublicHeader.h>
+
+
--- ViewCodeOne/ViewCodeOne/ViewController.swift
+++ ViewCodeOne/ViewCodeOne/ViewController.swift
@@ -13,7 +13,41 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func loadView() {
+        //load view here
+    }
 
 
+}
+
+//@available(iOS 13, *)
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct UIViewControllerPreview<ViewController: UIViewController>: UIViewControllerRepresentable {
+    let viewController: ViewController
+    
+    init(_ viewController: ViewController) {
+        self.viewController = viewController
+    }
+    
+    func makeUIViewController(context: Context) -> UIViewController {
+        viewController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        
+    }
+    
+}
+
+#endif
+
+struct ViewControllerPreviews: PreviewProvider {
+    static var previews: some View {
+        UIViewControllerPreview(ViewController()
+        )
+    }
 }
 
